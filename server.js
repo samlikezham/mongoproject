41a201544ccb8572eab0bb70b0b47d334a96b54a
Sam <schung015@gmail.com>
2018-07-06 08:53:01 -0700

    mongoose installed

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+// git push heroku master and git push origin master
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const mongoose = require('mongoose');
+const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
 
 
 app.get('/', (req, res) => {
@@ -10,4 +14,9 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
 	console.log('listening on port', PORT)
+});
+
+mongoose.connect(mongoUri);
+mongoose.connection.on('open', () => {
+	console.log('connected to mongoose!!!!!!!!');
 });
