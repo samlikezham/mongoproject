1be9323a67ad5643763f3a1a4b6a531c0df116ee
Sam <schung015@gmail.com>
2018-07-06 07:45:48 -0700

    express running

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('hello')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+	res.send('this works');
+});
+
+
+app.listen(port, () => {
+	console.log('listening on port', port)
+});
